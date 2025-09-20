4054678e49c4b91058af48c7b913af247bb6fba0
SK EJAJ <vk1eight18@gmail.com>
2025-09-20 21:58:53 +0530

    add frontend live link

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://url-shortner-wsue.vercel.app", 
   methods: ["GET", "POST"],
   credentials: true
 }));
